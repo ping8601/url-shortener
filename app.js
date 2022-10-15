7ecced94d0df5d47d7499eed3d7420ccbb49fb35
ping8601 <wanping86701@gmail.com>
2022-10-15 20:50:37 +0800

    set router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 require('./config/mongoose')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
@@ -12,9 +13,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 // set routes
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(port, () => [
   `Express is listening on http://localhost:${port}...`
